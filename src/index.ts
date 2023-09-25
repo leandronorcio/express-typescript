d9f124a9b25d527d2c4981697f5bcf90dcf9605b
Leandro Norcio <onsongnorcio@gmail.com>
2023-09-26 00:41:32 +0800

    Specify host.

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello Worldasd!');
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port: ${port}`);
+  console.log(`Listening on port: http://localhost:${port}`);
 });
